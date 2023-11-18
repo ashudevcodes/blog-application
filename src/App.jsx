3489c4bb5a48ba8357beb9dbe14cf46d0998ae77
Ashishprasa <ashishprasad949@gmail.com>
2023-11-18 11:55:48 +0530

    just add some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-function App() {
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from "./components"
 
-  return (
-    <>
-    
-    </>
-  )
+function App() {
+  const [loading, setloding] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.grtCurrentuser().then((userdata) => {
+      if (userdata) {
+        dispatch(login({ userdata }))
+      } else {
+        dispatch(logout())
+      }
+    }).finally(setloding(false))
+  },[])
+
+
+  return !loading ? <div className="min-h-screen flex flex-wrap content-between bg-gray-400"><div className="w-full block"><Header />
+  <main>YOU LOG IN</main>
+    <Footer />
+
+  </div></div> : <div>Loading... </div>
 }
 
 export default App
